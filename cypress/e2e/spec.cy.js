642e2522f1482165d38b5602f2384847693b7ddf
Alina Marasca <75176258+alinamarasca@users.noreply.github.com>
2023-01-18 10:35:51 +0100

    ui tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,12 +1,19 @@
 describe("test CRUD", () => {
-  describe("creates a new pet", () =>
-    it("successfully creates a new pet", () => {
+  describe("creates a new pet", () => {
+    it("shows error if invalid JSON", () => {
       cy.visit("/addPet");
-      cy.contains("Try it out").click();
-      cy.get("textarea")
-        .clear()
-        .type(
-          `{leftArrow} "id": 101,
+      cy.openInput();
+      cy.get("textarea").clear().type(`{leftArrow} invalid json`);
+      cy.contains("Execute").click();
+      cy.checkErrorMessage("Parameter string value must be valid JSON");
+    }),
+      it("successfully creates a new pet if valid JSON", () => {
+        cy.visit("/addPet");
+        cy.openInput();
+        cy.get("textarea")
+          .clear()
+          .type(
+            `{leftArrow} "id": 101,
           "category": {
             "id": 0,
             "name": "string"
@@ -22,25 +29,53 @@ describe("test CRUD", () => {
             }
           ],
           "status": "available"`
-        );
-      cy.contains("Execute").click();
-      cy.getServerResponse(200);
-    }));
+          );
+        cy.contains("Execute").click();
+        cy.getServerResponse(200);
+      }),
+      // questionable behavior. if it was real test case, I would check in with user-stories/developers/?
+      // to learn which behavior is expected => write test accordingly: to check if it creates a pet or shows error
+      // for now, I will assume it had to show error message => TEST WILL NOT PASS
+      it("shows error if empty JSON", () => {
+        cy.visit("/addPet");
+        cy.openInput();
+        cy.get("textarea").clear().type(`{leftArrow}`);
+        cy.contains("Execute").click();
+        cy.checkErrorMessage("Required data is not provided");
+      });
+  });
 
-  describe("reads a new pet", () => {
+  describe("read a new pet", () => {
     it("successfully finds pet by id", () => {
       cy.visit("/getPetById");
-      cy.contains("Try it out").click();
+      cy.openInput();
       cy.get("input[placeholder='petId']").type("101");
       cy.contains("Execute").click();
       cy.getServerResponse(200);
     });
+
+    it("shows error if no id provided", () => {
+      cy.visit("/getPetById");
+      cy.openInput();
+      cy.get("input[placeholder='petId']").invoke("val", "");
+      cy.contains("Execute").click();
+      cy.checkErrorMessage("Required field is not provided");
+    });
+
+    // again, I would check in with documentation, but will assume for now en error should have been shown => TEST WILL NOT PASS
+    it("shows error if wrong/non existent id provided", () => {
+      cy.visit("/getPetById");
+      cy.openInput();
+      cy.get("input[placeholder='petId']").type(0);
+      cy.contains("Execute").click();
+      cy.checkErrorMessage("This id does not exist, try another one");
+    });
   });
 
-  describe("updates a new pet", () => {
+  describe("updates a pet", () => {
     it("successfully updates a pet", () => {
       cy.visit("/updatePet");
-      cy.contains("Try it out").click();
+      cy.openInput();
       cy.get("textarea")
         .clear()
         .type(
@@ -63,13 +98,28 @@ describe("test CRUD", () => {
         );
       cy.contains("Execute").click();
       cy.getServerResponse(200);
+    }),
+      it("shows error if invalid JSON", () => {
+        cy.visit("/updatePet");
+        cy.openInput();
+        cy.get("textarea").clear().type(`{leftArrow} invalid json`);
+        cy.contains("Execute").click();
+        cy.checkErrorMessage("Parameter string value must be valid JSON");
+      });
+    // again, I would check in with documentation, but will assume for now en error should have been shown => TEST WILL NOT PASS
+    it("shows error if empty JSON", () => {
+      cy.visit("/updatePet");
+      cy.openInput();
+      cy.get("textarea").clear().type(`{leftArrow}`);
+      cy.contains("Execute").click();
+      cy.checkErrorMessage("Required data is not provided");
     });
   });
 
   describe("deletes pet by id", () => {
     it("successfully deletes pet by id", () => {
       cy.visit("/deletePet");
-      cy.contains("Try it out").click();
+      cy.openInput();
       cy.get("input[placeholder='petId']").type("101");
       cy.contains("Execute").click();
       cy.getServerResponse(200);
@@ -77,10 +127,27 @@ describe("test CRUD", () => {
 
     it("returns 404 when removed pet is requested", () => {
       cy.visit("/getPetById");
-      cy.contains("Try it out").click();
+      cy.openInput();
       cy.get("input[placeholder='petId']").type("101");
       cy.contains("Execute").click();
       cy.getServerResponse(404);
+    });
+
+    it("shows error if no id is provided", () => {
+      cy.visit("/deletePet");
+      cy.openInput();
+      cy.get("input[placeholder='petId']").invoke("val", "");
+      cy.contains("Execute").click();
+      cy.checkErrorMessage("Required field is not provided");
+    });
+
+    // again, I would check in with documentation, but will assume for now en error should have been shown => TEST WILL NOT PASS
+    it("shows error if empty JSON", () => {
+      cy.visit("/deletePet");
+      cy.openInput();
+      cy.get("textarea").clear().type(`{leftArrow}`);
+      cy.contains("Execute").click();
+      cy.checkErrorMessage("This id does not exist, try another one");
     });
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,16 @@
 Cypress.Commands.add("getServerResponse", expectedResponse => {
-  cy.get(".live-responses-table")
-    .find(".response")
-    .find(".response-col_status")
-    .contains(expectedResponse);
+  cy.get(".response-col_status").contains(expectedResponse);
+  cy.get(".responses-inner").find(".curl-command");
+  cy.get(".responses-inner").find(".microlight");
+  cy.get(".responses-inner").find(".live-responses-table");
+  cy.get(".response-col_description").find(".highlight-code");
+  cy.get(".response-col_description").find(".microlight");
+});
+
+Cypress.Commands.add("openInput", () => {
+  cy.contains("Try it out").click();
+});
+
+Cypress.Commands.add("checkErrorMessage", errorMessage => {
+  cy.get(".validation-errors").find("ul").find("li").contains(errorMessage);
 });
